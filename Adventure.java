864257089a6dceb2bfb803f125441cc290507b08
marianaacz <mariana-az@hotmail.com>
2018-10-23 19:46:23 -0600

    Create Adventure.java

---
+++ Adventure.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import javax.sql.RowSetInternal;
+import java.io.*;
+import java.util.*;
+
+class Adventure {
+    public static void main(String[] args) {
+        Map map1 = new Map();
+    }
+}
